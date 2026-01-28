4df42096832d6808c0b9c676d5fc221c15917542
jburg379 <joeburgos123@hotmail.com>
2026-01-27 20:32:28 -0500

    changed the aws proder version to match Coalfire's module requirements. Also added the tls and local providers

--- provider.tf
+++ provider.tf
@@ -1,8 +1,19 @@
+#sets AWS as the provider in which the infrastructure will be deployed
 terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "6.28.0"
+      version = "~> 5.85"  # This uses aws provider 5.15.x through 5.99.x to stay within the limits of Coalfire's module requirements
+    }
+
+    tls = {
+      source = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+
+    local = {
+      source = "hashicorp/local"
+      version = "~> 2.0"
     }
   }
 }
